ee627cf34faf924be97b0fb75e7909293202cd7d
Jere Kapyaho <2261936+jerekapyaho@users.noreply.github.com>
2025-01-27 10:27:08 +0200

    Lisätty sähkölaskuesimerkki

--- bonus/sahkolasku.py
+++ bonus/sahkolasku.py
@@ -1,14 +1,14 @@
 teksti = input('Anna sähkönkulutus kuukaudessa (kWh): ')
-kulutus = int(teksti)
+kulutus = float(teksti)
 
-myynnin_perusmaksu = 179   # snt/kk
-energiamaksu = 6.29 # snt/kWh
-siirron_perusmaksu = 398 # snt/kk
-siirron_energiamaksu = 3.1868 # snt/kWh
+myynnin_perusmaksu = 198.0   # snt/kk
+energiamaksu = 7.09 # snt/kWh
+siirron_perusmaksu = 477.0 # snt/kk
+siirron_energiamaksu = 2.9264 # snt/kWh
 vero = 2.79372 # snt/kWh
 
 energia = myynnin_perusmaksu + (kulutus * energiamaksu)
 siirto = siirron_perusmaksu + (kulutus * siirron_energiamaksu) + (vero * kulutus)
 summa = (energia + siirto) / 100.0
 
-print(f'Laskun summa = {round(summa, 2)} euroa')
+print(f'Laskun yhteissumma = {round(summa, 2)} euroa')
